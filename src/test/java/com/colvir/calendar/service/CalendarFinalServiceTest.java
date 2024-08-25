ae98e2874e1ee2ded29d1ec28b5daa069b8a87ab
akladkevich <andrukladkevich@gmail.com>
2024-08-25 17:11:15 +0300

    Тест для обработки переносов.

--- src/test/java/com/colvir/calendar/service/CalendarFinalServiceTest.java
+++ src/test/java/com/colvir/calendar/service/CalendarFinalServiceTest.java
@@ -4,6 +4,7 @@ import com.colvir.calendar.config.Config;
 import com.colvir.calendar.config.RabbitConfig;
 import com.colvir.calendar.rabbitmq.Producer;
 import com.colvir.calendar.repository.CalendarFinalMonthsRepository;
+import com.colvir.calendar.repository.CalendarFinalTransitionsRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,6 +33,9 @@ public class CalendarFinalServiceTest {
     @MockBean
     private CalendarFinalMonthsRepository calendarFinalMonthsRepository;
 
+    @MockBean
+    private CalendarFinalTransitionsRepository calendarFinalTransitionsRepository;
+
     @Test
     void processCalendarOriginal_success() {
 
@@ -51,8 +55,14 @@ public class CalendarFinalServiceTest {
 
         //Начало теста
         Assertions.assertDoesNotThrow(() -> calendarFinalService.processCalendarOriginal(calendarDataString, country));
+        // Обработка месяцев
         verify(calendarFinalMonthsRepository, times(12)).findAllByCountryAndYearAndMonthAndIsArchived(any(), any(), any(), any());
         verify(calendarFinalMonthsRepository, times(12)).save(any());
         verifyNoMoreInteractions(calendarFinalMonthsRepository);
+        // Обработка переносов
+        verify(calendarFinalTransitionsRepository).findAllByCountryAndYearAndIsArchived(any(), any(), any());
+        verify(calendarFinalTransitionsRepository, times(5)).save(any());
+        verifyNoMoreInteractions(calendarFinalTransitionsRepository);
+        // TODO: 25.08.2024 Обработка статистики
     }
 }
