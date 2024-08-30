404945fef606257ea0011228dd44d768a612a4df
akladkevich <andrukladkevich@gmail.com>
2024-08-30 19:25:57 +0300

    Правка скрипта liquibase.

--- src/main/resources/liquibase/scripts/init.sql
+++ src/main/resources/liquibase/scripts/init.sql
@@ -16,17 +16,10 @@ create table public.calendar_original
                    (ARRAY [('NEW'::character varying)::text, ('PROCESSED'::character varying)::text]))
 );
 
-alter table public.calendar_original
-    owner to postgres;
-
 create index calendar_original_country_year_is_archived_index
     on public.calendar_original (country, year, is_archived);
 
-
 create sequence public.sequence_calendar_original_id;
-
-alter sequence public.sequence_calendar_original_id owner to postgres;
-
 
 create table public.calendar_final_month
 (
@@ -40,17 +33,10 @@ create table public.calendar_final_month
     is_archived boolean
 );
 
-alter table public.calendar_final_month
-    owner to postgres;
-
 create index calendar_final_month_country_year_month_is_archived_index
     on public.calendar_final_month (country, year, month, is_archived);
 
-
 create sequence public.sequence_calendar_final_month_id;
-
-alter sequence public.sequence_calendar_final_month_id owner to postgres;
-
 
 create table calendar_final_transition
 (
@@ -63,9 +49,6 @@ create table calendar_final_transition
     day_to      varchar(255),
     is_archived boolean
 );
-
-alter table calendar_final_transition
-    owner to postgres;
 
 create index calendar_final_transition_country_year_day_from_is_archived_ind
     on calendar_final_transition (country, year, day_from, is_archived);
@@ -82,9 +65,6 @@ create table calendar_final_statistic
     is_archived boolean,
     workdays    integer
 );
-
-alter table calendar_final_statistic
-    owner to postgres;
 
 create index calendar_final_statistic_country_year_is_archived_index
     on calendar_final_statistic (country, year, is_archived);
