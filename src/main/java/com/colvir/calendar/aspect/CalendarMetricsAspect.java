05c45b04aacac029822649dac4c44ac21c01756b
akladkevich <andrukladkevich@gmail.com>
2024-09-01 00:23:03 +0300

    Добавлена метрика calendar_load_execution_time - время загрузки календаря.

--- src/main/java/com/colvir/calendar/aspect/CalendarMetricsAspect.java
+++ src/main/java/com/colvir/calendar/aspect/CalendarMetricsAspect.java
@@ -2,12 +2,15 @@ package com.colvir.calendar.aspect;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.AllArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
@@ -17,10 +20,15 @@ public class CalendarMetricsAspect {
     private final MeterRegistry meterRegistry;
 
     @Pointcut("within(com.colvir.calendar.controller.CalendarController)")
-    public void serviceMethods() {
+    public void сalendarControllerMethods() {
     }
 
-    @Around("serviceMethods()")
+    @Pointcut("execution(public * com.colvir.calendar.service.CalendarOriginalService.loadCalendarOriginalAll())")
+    public void loadCalendarOriginalAllMethod() {
+    }
+
+    // Мониторинг вызова методов контроллера CalendarController
+    @Around("сalendarControllerMethods()")
     public Object logCallCalendarControllerMethods(ProceedingJoinPoint joinPoint) {
 
         String methodName = joinPoint.getSignature().getName();
@@ -33,6 +41,25 @@ public class CalendarMetricsAspect {
             return joinPoint.proceed();
         } catch (Throwable e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    // Мониторинг времени загрузки календаря
+    @Around("loadCalendarOriginalAllMethod()")
+    public Object logTimeExecutionloadCalendarOriginalAll(ProceedingJoinPoint joinPoint) {
+
+        long startTime = System.nanoTime();
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        } finally {
+            long endTime = System.nanoTime();
+            String metricName =  "calendar_load_execution_time";
+            Timer.builder(metricName)
+                    .description("Execution time")
+                    .register(meterRegistry)
+                    .record(endTime - startTime, TimeUnit.NANOSECONDS);
         }
     }
 }
